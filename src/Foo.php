e7c62b317fbc5d4627ba73117d235cc0c936047e
asciito <ayax.cordova@aydev.mx>
2023-10-07 21:14:07 -0600

    change Foo.php

--- src/Foo.php
+++ src/Foo.php
@@ -4,6 +4,11 @@ namespace Asciito\LearningGithubActions;
 
 class Foo
 {
+    /**
+     * Get the current class name
+     * 
+     * @return string
+     */
     public static function className(): string
     {
         return static::class;
